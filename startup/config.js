0e93a62f3c3a84da2172c521504810177755facc
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-12-19 19:36:43 +0200

    recommit

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = function() {
-  if (!vidly_jwtPrivateKey) {
+  if (!process.env.vidly_jwtPrivateKey) {
     throw new Error('FATAL ERROR: vidly_jwtPrivateKey is not defined.');
   }
 }
--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 module.exports = function() {
-  const db = DATABASE_URL
+  const db = process.env.DATABASE_URL
   mongoose.connect(db)
     .then(() => console.log(`Connected to mongoDB Atlas...`))
     .catch(err => console.error(err))
