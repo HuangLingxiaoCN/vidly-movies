c081b8236f41b0e320bdf041fa091d1350299301
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-12-19 17:59:20 +0200

    change to real db

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 module.exports = function() {
-  const db = process.env.DATABASE_URL_TEST
+  const db = process.env.DATABASE_URL
   mongoose.connect(db)
     .then(() => console.log(`Connected to ${db}...`))
     .catch(err => console.error(err))
