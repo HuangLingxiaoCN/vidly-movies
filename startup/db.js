1ebd36a2e019546f6d828c7ca1742a6870b01d22
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-12-19 20:01:59 +0200

    recommit

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 module.exports = function() {
-  const db = process.env.DATABASE_URL
+  const db = process.env.db
   mongoose.connect(db)
     .then(() => console.log(`Connected to mongoDB Atlas...`))
     .catch(err => console.error(err))
